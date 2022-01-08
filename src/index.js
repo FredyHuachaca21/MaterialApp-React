dcbb51a6f6af4a9a81bab13630abbefab4e274f3
Fredy <fredy.huachaca21@gmail.com>
2022-01-08 13:06:08 -0500

    Counter app con Evento onClick

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimerApp';
 import './index.css'
+import CounterApp from './CounterApp';
 
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<PrimeraApp saludo='Soy Fredy' />, divRoot);
+ReactDOM.render(<CounterApp value={120} />, divRoot);
