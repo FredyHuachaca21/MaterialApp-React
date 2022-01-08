1e2f92add3a56f88d9db83c5ce18874f547af400
Fredy <fredy.huachaca21@gmail.com>
2022-01-08 11:26:27 -0500

    Agegando defaultProps de un argumento

--- src/PrimerApp.js
+++ src/PrimerApp.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 
 
 
-const PrimeraApp = ({saludo} )=>{
+const PrimeraApp = ({saludo, subtitulo} )=>{
 
     return(
     <>
         <h1>{saludo}</h1>
-
+        <p>{subtitulo}</p>
     </>
     );
 
@@ -17,6 +17,10 @@ const PrimeraApp = ({saludo} )=>{
 
 PrimeraApp.propTypes ={
     saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'soy un subtitulo'
 }
 
 export default PrimeraApp;
